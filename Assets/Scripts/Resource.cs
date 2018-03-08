8d60c14f91806ed747842947501cae468d6ef0ae
Milly <milabits@gmail.com>
2018-03-08 15:37:41 +0100

    Resources Workshop

---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Resource : MonoBehaviour
+{
+
+    public int quantity;
+    public int initialQuantity;
+    public UnityEvent OnValueChanged = new UnityEvent();
+
+
+    public void Awake()
+    {
+        quantity = initialQuantity;
+    }
+
+    public void AddAmount(int value)
+    {
+        quantity += value;
+
+        OnValueChanged.Invoke();
+    }
+
+    public void RemoveAmount(int value) {
+        quantity -= value;
+
+        OnValueChanged.Invoke();
+    }
+
+    public void SetAmount(int value)
+    {
+        quantity = value;
+
+        OnValueChanged.Invoke();
+    }
+}
---
+++ Assets/Scripts/ResourceUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Resource))]
+public class ResourceUI : MonoBehaviour {
+    public Text label;
+    public Text value;
+
+    private Resource resource;
+    
+    void Start() {
+        resource = GetComponent<Resource>();
+        label.text = resource.name;
+        
+        resource.OnValueChanged.AddListener(UpdateUI);
+
+        UpdateUI();
+    }
+
+    public void UpdateUI() {
+        value.text = resource.quantity.ToString();
+    }
+}
